4a33318f4dc63527f315bcfe3bb940efa457068a
raul <192793@iberopuebla.mx>
2023-01-20 23:03:00 -0600

    first commit

---
+++ Asignacion_1.py
@@ -0,0 +1,18 @@
+#Asignacion_1.py 
+# Generación de números aleatorios
+#Implementar una función o métodopara que se generen números aleatorios con las siguientes restricciones:
+#    Que el usuario proporcione cómo datos de entrada el rango en el que desea generar estos numeros (por ejemplo: números entre 0 y 20,000) 
+#    Que indique cuantos números desea generar (por ejemplo: 500 numero).
+#Estos números aleatorios deben almacenarse en un arreglo dinámico.
+
+#Modulo para generar numeros aleatorios
+import random
+
+#Funcion que genera una lista aleatoria, recibe 3 valores: size, el tamaño de la lista; being el inicio del rango de los numeros; y end, el final del rango de numeros aleatorios
+def random_list(size, begin=0, end=1000):
+    lista = [random.randint(begin,end)for x in range (size)]
+    return lista
+
+if __name__=="__main__":
+    print(random_list(20))
+    
---
+++ Asignacion_2.py
@@ -0,0 +1,26 @@
+# Asignacion_2.py
+# Implementación de algoritmo de Burbuja
+#Esta asignación consiste en agregar al código lo siguiente:
+#Implementar el algoritmo de ordenamiento  de  Burbuja.
+#La función debe recibir com parámetro de entrada el arreglo o lista a ordenar. 
+#Badillo Lora Raúl
+
+#Modulo que contiene la funcion para generar una lista con valores aleatorios
+import Asignacion_1 as r
+
+#Regrsa una lista ordenada por el algoritmo burbuja, recibe una lista
+def burbuja(lista):
+    for x in range(len(lista)-2):
+        for y in range(len(lista)-1,-1,-1):
+            if y ==x:
+                break
+            if lista[y]< lista[y-1]:
+                temp, lista[y] = lista[y], lista[y-1]
+                lista[y-1] = temp
+    return lista
+        
+        
+if __name__=="__main__":
+    l=r.random_list(20)
+    print(l)
+    print(burbuja(l))
