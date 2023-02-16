56be1e25652df9ea26ddb36f1180a4140b98294f
raul <192793@iberopuebla.mx>
2023-02-16 09:05:09 -0600

    completo

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import Asignacion_5 as qs
 import Asignacion_6 as sc
 import burbuja_s as bb
 import threading
-
+import pandas as pd
 import sys
  
 
@@ -31,10 +31,10 @@ for s in size:
         t1s = timeit.default_timer()
         ts +=(t1s-t0s)
         #print("Calculando quicksort")
-        # t0q = timeit.default_timer()
-        # b =qs.quicksort(l)
-        # t1q = timeit.default_timer()
-        # tq +=(t1q-t0q)
+        t0q = timeit.default_timer()
+        b =qs.quicksort(l,0,len(l)-1)
+        t1q = timeit.default_timer()
+        tq +=(t1q-t0q)
         #print("Calculando burbuja")
         t0b = timeit.default_timer()
         c =bb.burbuja(l)
@@ -43,8 +43,13 @@ for s in size:
     #print("calculando promedios")
     ps = ts/5
     ls.append(ps)
-    #lq.append(tq/5)
+    lq.append(tq/5)
     lb.append(tb/5)
-print(ls,lq,lb)
+df = pd.DataFrame({'burbuja':lb,
+                     'sacudida': ls,
+                     'quicksort':lq})
+
+df.to_csv('ordenamiento.csv', index=False)
+
 
 
