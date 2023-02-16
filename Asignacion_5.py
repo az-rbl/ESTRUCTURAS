e6d9fac7d8403eff66a2ccc3e6e8f9bb215fc565
raul <192793@iberopuebla.mx>
2023-02-16 13:26:29 -0600

    dafs

--- Asignacion_5.py
+++ Asignacion_5.py
@@ -4,51 +4,52 @@
 #Modulo que contiene la funcion para generar una lista con valores aleatorios
 import Asignacion_1 as r
 #Modulo con cronometro para medir el tiempo de ejecucion
-import timeit
-import sys
-sys.setrecursionlimit(60000)
+import time
+
 #Funcion para dividir la lista, encuentra un pivote en medio de la lista, mientras pone los valores pequeños al inicio del arreglo
-def particion(lista):
-    x = lista[0]
-    print(x)
-    i = 1
-    j = lista.index(lista[-1])
-    print(lista[j])
-    print(lista)
-    while(True):
-        while((lista[j]<= lista[0])==False):
-            #print(f"{lista[j]} is bigger than {lista[0]}")
-            j -=1
-        while((lista[i]>=lista[0])==False and i<len(lista)-1):
-            #print(f"{lista[i]} is smaller than {lista[0]}")
-            i +=1
-        if i < j:
-            #print(lista)
+def particion(lista,p,r):
+    x = lista[r]
+    i = p-1
+    j = p
+    while j <= r-1:
+        if lista[j] <= x:
+            i = i + 1
             temp =lista[i]
             lista[i]  = lista[j]
             lista[j] = temp
-            print(lista)
-        
-        #Pivote para dividir la lista
-        else:
-            #print(lista) 
-            return j
+        j = j + 1
+    temp =lista[i+1]
+    lista[i+1]  = lista[r]
+    lista[r] = temp
+    return i+1
+
 #Regresa una lista ordenada por el algoritmo quicksort, recibe una lista
-def quicksort(lista):
-    if len(lista)>1:
-        q=particion(lista)
-        lista[:q]=quicksort(lista[:q])
-        lista[q+1:]=quicksort(lista[q+1:])
-    #print(lista)
-    return(lista)
-            
+def quicksort(lista, p , r ):
+    if p < r:
+        q = particion(lista, p,r)
+        quicksort(lista,p, q-1)
+        quicksort(lista, q+1, r)
+
+
+
 if __name__=="__main__":
-    l=r.random_list(10)
+    tam = 500000
+    import sys
+    print(sys.getrecursionlimit()) # Prints 1000
+    sys.setrecursionlimit(2000)
+    print(sys.getrecursionlimit())
+
+    l=r.random_list(tam)
     #print(l)
-    t_0 = timeit.default_timer()
-    b = quicksort(l)
-    t_1 = timeit.default_timer()
-    print(t_1-t_0)
-    print(b)
-                
-    
+    t_0 = time.time()
+
+    print('tiempo_1:', t_0)
+    print('tamaño de la lista:', len(l))
+
+    quicksort(l,0,len(l)-1)
+
+    t_1 = time.time()
+    print('tiempo_2:', t_1)
+
+    print('\n tiempo en milisegundos:', t_1-t_0)
+    #print(l)
