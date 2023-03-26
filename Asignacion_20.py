1cc39d909dd3188aa92a437d82e6c8713065789b
raul <192793@iberopuebla.mx>
2023-03-25 18:44:37 -0600

    Asignacion_20 entregada

--- Asignacion_20.py
+++ Asignacion_20.py
@@ -1,18 +1,30 @@
-#
+#Asignacion_20.py
+#Raúl Badillo Lora
+#Implementación de tablas hash ver-2
+#En esta versión de tabla hash se resuelve el problema de colisiones.
+#Se implementa una lista ligada para cada clave de entrada que genere colisión.
+# Además, se implementa una función para buscar un elemento ya insertado,
+# la función regresa como resultado si el elemento buscado esta en la estructura o nó.
 
 import Asignacion_1 as r
 
-
+#Clase que crea el objeto hash table, se inicializa con una lista de numeros en desorden
 class hash_table(list):
+    #inicializador, crea una lista valores de tamaño m= 2 * tamaño de la lista desordenada
+    #Se inicializa en cero el contador de colisiones
+    #Se usa la funcion insertar_lista para insertar en su lugar correspondiente a cada miembro de la lista desordenada
     def __init__(self,list):
         self.m = 2*len(list)
         self.valores= self.m*[None]
         self.colisiones = 0
         self.insertar_lista(list)
         
+    # Funcion de hash = modulo(x,m)
     def hash_function(self, x):
         return x%self.m
     
+    # Inserta cada valor en el indice correspondiente a su funcion hash.
+    # implementa una lista ligada para cada clave de entrada que genere colisión.
     def insertar(self, x):
         i = self.hash_function(x)
         if(self.valores[i]==None):
@@ -23,11 +35,13 @@ class hash_table(list):
         else:
             self.valores[i]= [self.valores[i],x]
             self.colisiones +=1
-    
+            
+    #Inserta en su lugar correspondiente a cada miembro de la lista desordenada
     def insertar_lista(self,list):
         for i in list:
             self.insertar(i)
-        
+    
+    # la función regresa como resultado si el elemento buscado esta en la estructura o nó.    
     def buscar(self, elemento):
         i= self.hash_function(elemento)
         if(self.valores[i]==elemento):
